af8ff55d634d7b471eb3a34ffa380ed1b8f28384
Shubhrato <shubhrato18@gmail.com>
2024-10-07 15:38:30 +0530

    Card project
    create card project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Component/card'
+
+function App() {
+  
+  
+  return (
+    <>
+      <Card Username="BMW M4"/>
+      <br/>
+      <Card Username="BMW M5"/>
+    </>
+  )
+}
+
+export default App
