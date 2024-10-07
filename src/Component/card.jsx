8e1d08739bec968992d2cd1884512f58dd76c8c6
Shubhrato <shubhrato18@gmail.com>
2024-10-07 16:35:32 +0530

    upgrade

--- src/Component/card.jsx
+++ src/Component/card.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 
 
-function Card({Username, Discription,href}) {
+function Card({Username, Discription,href, tag1, tag2, tag3}) {
     
     return (
       <>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-        <img class="w-full" src={href} alt="Card Image" />
-        <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">{Username}</div>
-            <p class="text-gray-700 text-base">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <img className="w-full" src={href} alt="Card Image" />
+        <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{Username}</div>
+            <p className="text-gray-700 text-base">
                 {Discription}
             </p>
         </div>
-        <div class="px-6 pt-4 pb-2">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#tag1</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#tag2</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#tag3</span>
+        <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tag1}</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tag2}</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tag3}</span>
         </div>
     </div>
       </>
